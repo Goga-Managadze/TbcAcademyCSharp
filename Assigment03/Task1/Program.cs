839c53572b109cbc2e916b08c12dc8d3b30043b0
Goga Managadze <gomanagadze@tbcbank.com.ge>
2023-06-07 14:46:35 +0400

    Assigment03_V1

---
+++ Assigment03/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1.დაწერეთ პროგრამა რომელსაც კონსოლიდან გადავცემთ რიცხვს და პასუხად დაბეჭდავს რიცხვი მარტივია თუ შედგენილი.
+int dividend = int.Parse(Console.ReadLine());
+int divisor = 1;
+int count = 0;
+
+while (divisor <= dividend)
+{
+    if (dividend % divisor == 0)
+    {
+        count += 1;
+    }
+    divisor += 1;
+}
+
+if (count == 2)
+{
+    Console.WriteLine("martivi ricxvi");
+}
+else if (dividend == 1)
+{
+    Console.WriteLine("1 ar aris arc martivi ricxvi da arc shedgenili");
+}
+else
+{
+    Console.WriteLine("shedgenili ricxvi");
+}
---
+++ Assigment03/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//2.დაწერეთ პროგრამა რომელსაც კონსოლიდან გადავცემთ რიცხვს და პასუხად დაბეჭდავს შეყვანილი რიცხვის გამყოფების რაოდენობას.
+//მაგალითად, თუ შევიყვანთ 6-ს, უნდა დაიბეჭდოს 4-ს (1, 6, 2, 3).
+
+int dividend = int.Parse(Console.ReadLine());
+int divisor = 1;
+int count = 0;
+
+while (divisor <= dividend)
+{
+    if (dividend % divisor == 0)
+    {
+        count += 1;
+        //Console.WriteLine("divisors are" + divisor);
+        Console.Write(divisor + ",");
+    }
+    divisor += 1;
+}
+Console.WriteLine("\n" + "Count of divisors: " + count);
