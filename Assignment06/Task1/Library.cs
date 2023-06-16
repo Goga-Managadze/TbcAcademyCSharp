e5eecf0294937cea56c50822c013bc28615e0b54
Goga Managadze <gomanagadze@tbcbank.com.ge>
2023-06-16 18:25:26 +0400

    Assignment06_V1

---
+++ Assignment06/Task1/Library.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    public class Library
+    {
+        private Book[] _books;
+        private int count = 0;
+
+        public Book this[int i]
+        {
+            get
+            {
+                return _books[i];
+            }
+            set {
+                _books[i] = value;
+            }
+        }
+
+        public int setBooks
+        {
+            set{
+                _books = new Book[value];
+            }
+            
+        }
+        public void AddBook(Book Book)
+        {
+            if (count < _books.Length)
+            {
+                _books[count] = Book;
+                count++;
+            }
+            Console.WriteLine($"Added Book Title: {Book.Title}, Author: {Book.Author}, Year: {Book.Year}, Genre: {Book.Genre}");
+        }
+        public void RemoveBook(string title)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (_books[i].Title == title)
+                {
+                    _books[i] = null;
+                    count--;
+                    Console.WriteLine("Book removed successfully.");
+                    break;
+                }
+            }
+        }
+        public Book this[string i]
+        {
+            get
+            {
+                for (int j = 0; j < _books.Length; j++)
+                {
+                    if (i == _books[j].Title)
+                    {
+                        Console.WriteLine("The book titled " + "'" + i + "'" + " has been found in the array");
+                        return _books[j];
+                    }
+                }
+                return null;
+            }
+            set { 
+            }
+        }
+
+        public void CountOfBooks()
+        {
+            Console.WriteLine("Count of books: " + count);
+        }
+    }
+}
---
+++ Assignment06/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Organizations1;
+using System;
+
+Organizations Organization = new Organizations { Name = "Organization", ActivityField = "Activity", Address = "Address" };
+Organizations Organization1 = new Organizations { Name = "Organization1", ActivityField = "Activity1", Address = "Address1" };
+Organizations Organization2 = new Organizations { Name = "Organization2", ActivityField = "Activity2", Address = "Address2" };
+Organizations Organization3 = new Organizations { Name = "Organization3", ActivityField = "Activity3", Address = "Address3" };
+
+OrganizationList organizationList = new OrganizationList();
+Organizations[] organizationsAddList = new Organizations[] { Organization, Organization1, Organization2, Organization3 };
+
+organizationList.setOrganizations = 10;
+organizationList.AddElement(Organization);
+organizationList.AddElement(Organization1);
+organizationList.AddElement(Organization2);
+organizationList.AddElement(Organization3);
+
+organizationList.GetElement(1);
+organizationList.AddList(organizationsAddList);
+
+//organizationList.RemoveElement(Organization2);
+
+//Organizations[] organizationsRemoveList = new Organizations[] { Organization2, Organization3 };
+//organizationList.RemoveList(organizationsRemoveList);
+//organizationList.ClearList();
+//organizationList.FindOrganization("Organization");
+//organizationList.OrganizationsCount();
