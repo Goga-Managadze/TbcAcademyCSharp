d964d4e15671e99f35569291d57e4a2ab291987a
Goga Managadze <gomanagadze@tbcbank.com.ge>
2023-07-21 16:53:37 +0400

    Assignment19_V1

---
+++ Assignment19/Task/ParallelCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class ParallelCalculator
+    {
+        public static long CalculateParallelSum(int[] array)
+        {
+            long sum = 0;
+            Parallel.For(0, array.Length, i =>
+            {
+                sum += array[i];
+            });
+            return sum;
+        }
+    }
+}
